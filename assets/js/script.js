4c2a4f76deeb3a092ffda1c3097e937cc55d7136
e-phraim <ephraimetuk85@gmail.com>
2025-03-07 12:07:49 +0100

    demo --mini gallery.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -192,16 +192,16 @@ rightBtn.addEventListener('mouseleave', stopMovement)
 //Start this baby up
 ;(function init() {
   var images = [
-    './assets/images/gallery1.jpeg',
-    './assets/images/gallery3.jpeg',
-    './assets/images/gallery4.jpeg',
-    './assets/images/gallery7.jpeg',
-    './assets/images/gallery6.jpeg',
-    './assets/images/gallery3.jpeg',
-    './assets/images/gallery4.jpeg',
-    './assets/images/gallery2.jpeg',
-    './assets/images/gallery8.jpeg',
-    './assets/images/gallery5.jpeg',
+    '../images/gallery1.jpeg',
+    '../images/gallery2.jpeg',
+    '../images/gallery3.jpeg',
+    '../images/gallery7.jpeg',
+    '../images/gallery6.jpeg',
+    '../images/gallery4.jpeg',
+    '../images/gallery4.jpeg',
+    '../images/gallery2.jpeg',
+    '../images/gallery8.jpeg',
+    '../images/gallery5.jpeg',
   ]
 
   //Set Initial Featured Image
